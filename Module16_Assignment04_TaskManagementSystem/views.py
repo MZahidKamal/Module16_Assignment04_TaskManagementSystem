7be97975197d4175653776d91c0c7bd926968065
MZahidKamal <kamal.md.zahid@gmail.com>
2023-12-09 12:50:14 +0100

    Daily Learning & Commit

--- Module16_Assignment04_TaskManagementSystem/views.py
+++ Module16_Assignment04_TaskManagementSystem/views.py
@@ -5,6 +5,7 @@ def base(request):
     return render(request, 'base.html')
 
 def home(request):
-    tasks = Tasks_Model.objects.all()
+    # tasks = Tasks_Model.objects.all()
+    tasks = Tasks_Model.objects.order_by('assign_date')
     context = {'tasks': tasks}
     return render(request, 'index.html', context)
